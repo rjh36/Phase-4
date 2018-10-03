5571036b2ff24e215c2623da654878c546d3382d
bjj13 <31720298+bjj13@users.noreply.github.com>
2018-10-03 15:50:39 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include('server.php'); ?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>User Registration System</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="header">
+            <h2>Home page</h2>
+        </div>
+        <div class="content">
+            <?php if(isset($_SESSION['success'])): ?>
+            <div class="error success">
+                <h3>
+                    <?php 
+                        echo $_SESSION['success'];
+                        unset($_SESSION['success']);
+                    ?>
+                </h3>
+            </div>
+            <?php endif ?>
+            
+            <?php if (isset($_SESSION["username"])): ?>
+                <p>Welcome <strong><?php echo $_SESSION['username'] ?></strong></p>
+                <p><a href="index.php?logout='l'" style="color: red;">Logout</a></p>
+            <?php endif ?>
+        </div>
+    </body>
+</html>
