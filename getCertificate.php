3c7fde0c38b4ba149b9880bd6cc3c9e9ddd6d140
Robert Hand <rjh36@students.uwf.edu>
2018-10-15 23:47:09 -0500

    Did some work on the layout of the get certificate page (part 2).

---
+++ getCertificate.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!-- <?php include('server.php'); ?> -->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Get Certificate</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="header">
+            <h2>Congratulations, how do you want your certificate?</h2>
+        </div>
+        <div class="btn-group">
+            <button name="print" class="btn">Print!</button>
+            <button name="download" class="btn">Download!</button>
+            <button name="email" class="btn">Email!</button>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
