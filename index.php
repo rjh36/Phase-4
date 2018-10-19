5f7926ba264cf751723f654414f2d8670c1b6eeb
Robert Hand <rjh36@students.uwf.edu>
2018-10-19 15:42:37 -0500

    Worked on the look of the home page.

--- index.php
+++ index.php
@@ -24,8 +24,34 @@
             <?php endif ?>
             
             <?php if (isset($_SESSION["username"])): ?>
-                <p>Welcome <strong><?php echo $_SESSION['username'] ?></strong></p>
-                <p><a href="index.php?logout='l'" style="color: red;">Logout</a></p>
+                <p>
+                    Welcome to Cyber Security training 2.0 <strong><?php echo $_SESSION['username'] ?></strong>! 
+                    This course has four modules: 
+                    Phishing and Social Engineering, 
+                    Patches and Antivirus, 
+                    Password Strength,
+                    and Public Wi-Fi Security.
+                    Feel free to take them in any order before the final exam!
+                </p>
+            <?php else: ?>
+                <p>
+                    Welcome to Cyber Security training 2.0! 
+                    You need to create an account and login before using the system.
+                </p>
+            <?php endif ?>
+        </div>
+        <div class="sidebar">
+            <?php if (isset($_SESSION["username"])): ?>
+                <a href="index.php?logout='l'" style="color: red;">Logout</a><br>
+                <a href="#" style="color: red;">Phishing and Social Engineering</a><br>
+                <a href="#" style="color: red;">Patches and Antivirus</a><br>
+                <a href="#" style="color: red;">Password Strength</a><br>
+                <a href="#" style="color: red;">Public Wi-Fi Security</a><br>
+                <a href="#" style="color: red;">Take Final Exam</a><br>
+                <a href="getCertificate.php" style="color: red;">Get Certificate</a>
+            <?php else: ?>
+                <a href="register.php" style="color: red;">Create Account</a><br>
+                <a href="login.php" style="color: red;">Login</a>
             <?php endif ?>
         </div>
     </body>
