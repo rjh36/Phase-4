4c876f6359eae5d83e20eb8d6e79aab7a038124e
vsg3 <vsg3@students.uwf.edu>
2018-10-24 19:52:59 -0500

    Add module1.php and fix style.css

---
+++ getCertificate.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<?php include('server.php'); ?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Get Certificate</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="header">
+            <h2>Congratulations, how do you want your certificate?</h2>
+        </div>
+        <div class="content">
+            <embed src="Templates/CertificateTemplate.pdf" width="100%" height="700px" type='application/pdf'>
+        </div>
+        <div class="sidebar">
+                <a href="index.php?logout='l'">Logout</a>
+                <a href="index.php">Home</a>
+                <a href="#">Email</a>
+        </div>
+        
+        <?php
+            // Code?
+        ?>
+    </body>
+</html>
