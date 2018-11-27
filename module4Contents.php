05a9e31385f1ac27c282699de8720755c02bcfde
bjj13 <bjj13@students.uwf.edu>
2018-11-26 19:26:36 -0600

    Create module4Contents.php

---
+++ module4Contents.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<div class="pages">
+    <div class="1">
+        <h3>Introduction</h3>
+        <p>
+            &emsp;Wi-Fi hotspots are available almost everywhere we go these days. Sure, 
+            they’re convenient when you’re on the go and need to get something done online, 
+            but they’re not always safe. In fact, there are many free, use-friendly tools 
+            available that enable any wannabe hacker to easily access vulnerable information 
+            floating around your local public wi-fi hotspot.
+        </p>
+        <p>
+            &emsp;In this module, we will cover rules and tips to keep you safe when using public wi-fi.
+        </p>
+    </div>
+    <div class="2">
+        <h3>A Message from the Federal Trade Commission</h3>
+        <iframe src='//players.brightcove.net/1087587715001/Vy0NWmo9x_default/index.html?videoId=2157883693001' allowfullscreen frameborder=0></iframe>
+    </div>
+    <div class="3">
+        <h3>Choosing the Right Wi-Fi Network</h3>
+        <p>
+            &emsp;As you saw in the video, it is always best to use an encrypted network if 
+            that is an option. When that is not an option, you need to make sure that the network 
+            you choose is a legitimate network. Hackers can set up their own wi-fi networks that 
+            look completely legitimate in order to trick people into using them instead. To avoid 
+            this, make sure to ask an employee for the name of the wi-fi network before you log on.
+        </p>
+    </div>
+    <div class="4">
+        <h4>VPNs</h4>
+        <p>
+            &emsp;If you are using a public wi-fi hotspot, then you really can’t be sure the network 
+            is encrypted and your information is safe. For this reason, you want to make sure you’re 
+            not transmitting any sensitive data – that is, any information you wouldn’t want a stranger 
+            to have. Additionally, wherever possible, use https instead of http. You can find a browser 
+            add-on to help make sure you always follow that rule.
+        </p>
+        <p>
+            &emsp;Additionally, connecting first to a Virtual Private Network (VPN) is a good idea. A VPN 
+            encrypts your data and routes it through a private server to your destination and does the same 
+            upon return from the site back to your computer. There are many free VPNS available, your company 
+            may even have one for you to use, or you can pay for one that may be a little better.
+        </p>
+    </div>
+    <div class="5">
+        <h4>Conclusion</h4>
+        <p>
+            Using a public wi-fi network can put you at risk! 
+        </p>
+        <p>
+            Follow the steps outlined below to help protect yourself:
+        </p>
+        <ul>
+            <li>
+                Make sure you’re on a legit network and not a fake one. Verify the name 
+                of the network with an employee.
+            </li>
+            <li>
+                Keep your security software up to date and make sure your Firewall is 
+                enabled.
+            </li>
+            <li>
+                Don’t use the same password for multiple accounts and use two-factor 
+                authentication when possible.
+            </li>
+            <li>
+                Turn off sharing in your Network Settings in your Control Panel. You can 
+                save these settings for public networks.
+            </li>
+            <li>
+                Encrypt your data whenever possible by using HTTPS. You can install add-ons 
+                for your browser like Force-TLS or HTTPS Everywhere that will do this automatically.
+            </li>
+            <li>
+                Turn off wi-fi when you’re not using it and disable the “connect automatically” 
+                feature so you can control when you’re on a public network.
+            </li>
+            <li>
+               Keep your browser and apps updated to ensure all known vulnerabilities have been patched.
+            </li>
+            <li>
+                Always log out of your accounts when you’re done using them.
+            </li>
+            <li>
+                For added protection, you can always route through a virtual private network (VPN) 
+                which is private and secure. You can download and install a VPN program at home.
+            </li>
+            <li>
+                For extra protection, don’t do anything you want to keep private on a public network. 
+                Better safe than sorry.
+            </li>
+        </ul>
+    </div>
+
