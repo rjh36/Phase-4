b50a27d8e8c8a815c157e3271d0d03ab741f1f08
MattSextonUSMC <mls105@students.uwf.edu>
2018-12-06 22:40:31 -0800

    Added the Unity Game

--- module3Contents.php
+++ module3Contents.php
@@ -134,12 +134,26 @@
     <div class="9">
         <h3>Game</h3>
         <br>
-        <!-- Game stuff here -->
+            <h3>Game</h3>
+                <script src="UnityCode/PatchesAndUpdates/TemplateData/UnityProgress.js"></script>  
+                <script src="UnityCode/PatchesAndUpdates/Build/UnityLoader.js"></script>
+                <script>
+                    var gameInstance = UnityLoader.instantiate("gameContainer", "UnityCode/PatchesAndUpdates/Build/PatchesAndUpdates.json", {onProgress: UnityProgress});
+                </script>
+                <div class="webgl-content">
+                    <div id="gameContainer" style="width: 960px; height: 600px"></div>
+                    <div class="footer">
+                        <div class="webgl-logo"></div>
+                        <div class="fullscreen" onclick="gameInstance.SetFullscreen(1)"></div>
+                        <div class="title">New Unity Project</div>
+                    </div>
+                </div>
         
         <div class="CompleteCourse">
             <form action="updateProgress.php" method="post" id="completeModule">
                 Click this to complete the course: <input type="submit" name="update" value="module3"/>
             </form>
         </div>
+        </br>
     </div>
 </div>
