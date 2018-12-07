df99f2e416b7380944e2b4c6349055c91b77a196
Robert Hand <rjh36@students.uwf.edu>
2018-12-07 13:04:20 -0600

    Removing duplicates of files.

--- updateProgress.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('server.php');
-
-// Completes the course
-if (isset($_POST['update'])) {
-    updateProgressTrue($db, $_SESSION["id"], $_POST['update']);
-    header('location: index.php'); //redirect to home page
-}
